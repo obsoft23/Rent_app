f1bc568c14299845925283fbc980913b9e9eb711
obsoft23 <holarmidhey@gmail.com>
2025-10-12 20:15:10 +0100

    app_first_stage

---
+++ lib/theme/constants.dart
@@ -0,0 +1,9 @@
+// ignore_for_file: unused_import
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:get/get.dart';
+import 'package:flutter/material.dart';
+
+
+FirebaseFirestore firestore = FirebaseFirestore.instance;
+
---
+++ lib/theme/theme.dart
@@ -0,0 +1,42 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+const kPrimaryColor = Colors.blue;
+
+//const kPrimaryColor = Color(0xFF1B383A);
+const kPrimaryColor2 = Colors.yellow;
+const kSecondaryColor = Color(0xFF59706F);
+const kSecondaryColor2 = Colors.blueAccent;
+const kDarkGreyColor = Color(0xFFA8A8A8);
+const kWhiteColor = Color(0xFFFFFFFF);
+const kZambeziColor = Color(0xFF5B5B5B);
+const kBlackColor = Color(0xFF272726);
+const kTextFieldColor = Color(0xFF979797);
+const kDefaultPadding = 28.0;
+const kTextColor = Color(0xFF1C1C1C);
+const kLightTextColor = Color(0xFF363636);
+const kAltTextColor = Color(0xFFBBBBBB);
+const kAltDarkTextColor = Color(0xFF9D9D9D);
+
+TextStyle titleText =
+    TextStyle(color: kPrimaryColor, fontSize: 28, fontWeight: FontWeight.w700);
+
+TextStyle titleText2 = TextStyle(
+    color: kSecondaryColor, fontSize: 22, fontWeight: FontWeight.w700);
+TextStyle subTitle = TextStyle(
+    color: kSecondaryColor, fontSize: 16, fontWeight: FontWeight.w500);
+TextStyle subTitle2 =
+    TextStyle(color: Color.fromARGB(255, 157, 74, 68), fontSize: 21, fontWeight: FontWeight.bold);
+TextStyle subTitle3 = TextStyle(
+    color: kSecondaryColor2, fontSize: 18, fontWeight: FontWeight.bold);
+TextStyle subTitle4 = TextStyle(
+    color: kSecondaryColor, fontSize: 12, fontWeight: FontWeight.w500);
+    TextStyle subTitle5 =
+    TextStyle(color: Color.fromARGB(255, 240, 51, 38), fontSize: 21, fontWeight: FontWeight.bold);
+
+TextStyle textButton = TextStyle(
+  color: kPrimaryColor,
+  fontSize: 18,
+  fontWeight: FontWeight.w700,
+);
